f1ccffb16b7641c1619b439bd94200984b0c919c
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2023-12-16 17:37:06 +0900

    Refactor command line argument parsing in ubs.sh

--- ubs.sh
+++ ubs.sh
@@ -17,66 +17,70 @@ function showHelpMessage() {
   echo "Controls the number of CPUs enabled/disabled or displays that information." >&2
   echo "" >&2
   echo "Specific Usage:" >&2
-  echo "  $(basename $0) enable NUMBER      Enables the specified number of CPUs, starting from CPU0, and disables the rest. Root required." >&2
-  echo "  $(basename $0) disable NUMBER     Disables the specified number of CPUs, starting from CPU1, and enables the rest. Root required." >&2
-  echo "  $(basename $0) info               Show the current CPU activation status." >&2
-  echo "  $(basename $0) help               Show this help message." >&2
+  echo "  $(basename $0) enable NUMBER     Enables the specified number of CPUs, starting from CPU0, and disables the rest. Root required." >&2
+  echo "  $(basename $0) disable NUMBER    Disables the specified number of CPUs, starting from CPU1, and enables the rest. Root required." >&2
+  echo "  $(basename $0) info              Show the current CPU activation status." >&2
+  echo "  $(basename $0) help              Show this help message." >&2
   echo "" >&2
   echo "Notes:" >&2
   echo "  - \`$(basename $0) NUMBER\` == \`$(basename $0) enable NUMBER\`." >&2
   echo "  - \`$(basename $0)\` == \`$(basename $0) info\`." >&2
-  #echo "  - To enable or disable the CPU, you must run as root." >&2
 }
 
-OPERATION=""
-ARG=""
+
 # Parse Arguments
 function parseArgs() {
+  local operation=""
+  local arg=""
   if [ $# -eq 0 ]; then
     # No arguments
-    OPERATION="info"
+    operation="info"
   elif [ $# -eq 1 ]; then
     # One Argument
     if [[ "$1" =~ ^[0-9]+$ ]]; then
-      OPERATION="enable"
-      ARG="$1"
+      operation="enable"
+      arg="$1"
     else
-      OPERATION="$1"
+      operation="$1"
     fi
   elif [ $# -eq 2 ]; then
     # Two Arguments
-    OPERATION="$1"
-    ARG="$2"
+    operation="$1"
+    arg="$2"
   else
     echo "Error: Too many arguments" >&2
     showShortHelpMessage
     exit 1
   fi
 
-  case "$OPERATION" in
+  case "$operation" in
     "info" | "help")
-      if [ ! -z "$ARG" ]; then
+      if [ ! -z "$arg" ]; then
         echo "Error: Too many arguments" >&2
         showShortHelpMessage
         exit 1
       fi
       ;;
     "enable" | "disable")
-      if [ -z "$ARG" ]; then
+      if [ -z "$arg" ]; then
         echo "Error: Missing argument" >&2
         showShortHelpMessage
         exit 1
       fi
       ;;
     *)
-      echo "Error: Invalid operation: $OPERATION" >&2
+      echo "Error: Invalid operation: $operation" >&2
       showShortHelpMessage
       exit 1
       ;;
   esac
+
+  echo "$operation" "$arg"
 }
 
-parseArgs "$@"
+result=($(parseArgs "$@"))
+operation="${result[0]}"
+arg="${result[1]}"
 
-echo "Operation is" "$OPERATION"
-echo "Argument is" "$ARG"
+echo "Operation is" "$operation"
+echo "Argument is" "$arg"
